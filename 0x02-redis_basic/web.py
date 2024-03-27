e93b263ae046c6b50ba8877215c6f24a4a45e2a0
Mulle21 <mulukenab95@gmail.com>
2024-03-27 12:40:15 -0700

    Implementing an expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,50 +1,32 @@
 #!/usr/bin/env python3
-"""
-Caching request module
-"""
+""" Redis Module """
+
+from functools import wraps
 import redis
-import uuid
-from typing import Union, Callable, Optional
+import requests
+from typing import Callable
+
+redis_ = redis.Redis()
 
 
-class Cache:
-    def __init__(self, host='localhost', port=6379, db=0):
-    """ Decorator for get_page
-    """
-    self._redis = redis.Redis(host=host, port=port, db=db)
-    self._redis.flushdb()
+def count_requests(method: Callable) -> Callable:
+    """ Decortator for counting """
+    @wraps(method)
+    def wrapper(url):  # sourcery skip: use-named-expression
+        """ Wrapper for decorator """
+        redis_.incr(f"count:{url}")
+        cached_html = redis_.get(f"cached:{url}")
+        if cached_html:
+            return cached_html.decode('utf-8')
+        html = method(url)
+        redis_.setex(f"cached:{url}", 10, html)
+        return html
 
-    def store(self, data: Union[str, bytes, int, float]) -> str:
-    """
-    """
-    key = str(uuid.uuid4())
-    self._redis.set(kry, data)
-    return key
+    return wrapper
 
-    def get(self, key: str,
-            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
-            """
-            """
-            data = self._redis.get(key)
-            if data is None:
-                return data
-            if fn:
-                callable_fn - fn(data)
-                return callable_fn
-            else:
-                return data
-        def get_str(self, key: str) -> str:
-            """
-            """
-            value = self._redis.get(key. fn=lambda d: d.decode()"utf-8"))
-            return value
-        def get_int(self, key: str) -> int:
-            """
-            """
-            value = self._redis.get(key)
-            try:
-                value = int(value.decode("utf-8"))
-            except Exception:
-                return None
 
-            return value
+@count_requests
+def get_page(url: str) -> str:
+    """ Obtain the HTML content of a  URL """
+    req = requests.get(url)
+    return req.text
